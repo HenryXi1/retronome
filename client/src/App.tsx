3c0b120bf08ed9b1d4a00cc6a188e2b6feff6d95
Henry Xi <henryxi@Henrys-MacBook-Air.local>
2025-09-13 00:51:35 -0400

    use react ts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
-function App() {
+import React from 'react';
+
+function App(): React.JSX.Element {
   return (
     <div>
       <h1>Hello React!</h1>
       <p>Your React app is ready to go.</p>
     </div>
-  )
+  );
 }
 
 export default App
